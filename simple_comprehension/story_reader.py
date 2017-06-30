96980e674d74f58e06aefd3526e91432bcebdd17
Haemanth S P <haemanth@digitalaristotle.ai>
2017-06-30 07:04:11 +0530

    Extract partial information from story
    Very basic subject verb object extraction from a given story
    implemented. Pronoun resolution need to be done.

---
+++ simple_comprehension/story_reader.py
@@ -0,0 +1,54 @@
+import spacy
+import networkx as nx
+import io
+from networkx_viewer import Viewer
+
+def plot(input_graph):
+    app = Viewer(input_graph)
+    app.mainloop()
+
+# Read the story form file
+with io.open('clever_thief.txt', 'r', encoding='utf8') as ip_file:
+    story = ip_file.read()
+
+# Initialize a graph
+g_story = nx.Graph()
+
+
+# Simple pronoun resolution
+
+# Get triplets from the story
+nlp = spacy.load('en')
+story_obj = nlp(story)
+for sent in story_obj.sents:
+    subs, verb, objs = [], '', []
+    for word in sent:
+      if word.dep_ == 'nsubj':
+          subs.append(word.text)
+      if word.dep_ == 'ROOT':
+          verb = word.text
+      if word.dep_ in ['dobj', 'attr', 'acomp']:
+          objs.append(word.text)
+    print 'S/V/O:', subs, verb, objs
+    if subs and verb and objs:
+	for subj in subs:
+            for obj in objs:
+                g_story.add_edge(subj, obj, relation=verb)
+print g_story.edges(data=True)
+
+question = ''
+while question!=unicode('exit'):
+    question = unicode(raw_input())
+    ques_obj = nlp(question)
+    for sent in ques_obj.sents:
+        subs, verb, objs = [], '', []
+        for word in sent:
+          if word.dep_ == 'nsubj':
+              subs.append(word.text)
+          if word.dep_ == 'ROOT':
+              verb = word.text
+          if word.dep_ in ['dobj', 'attr', 'acomp']:
+              objs.append(word.text)
+        print 'S/V/O:', subs, verb, objs
+        q = objs + subs
+	print g_story.edges(q[0], data=True)
